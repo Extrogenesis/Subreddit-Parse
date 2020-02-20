8286212a02541b81f956dec646102923b4173dfe
Extrogenesis <extrogenesis@gmail.com>
2020-02-20 14:32:53 -0500

    init commit

---
+++ init.py
@@ -0,0 +1,32 @@
+"""
+
+1. Confirmed saved file is HTML
+2. Parse and return links
+3. Confirm if link is a subreddit
+
+"""
+
+
+
+import os 
+
+cwd = os.getcwd()  # current working directory
+
+os.listdir(cwd)  # list contents in dir - return an array. Example: os.listdir('/') would list everything in the root dir
+
+
+def find_HTML():
+    pass
+
+def confirm_file_is_HTML():
+    pass
+
+def find_links():
+    pass
+
+def is_link_a_subreddit():
+    pass
+
+if __name__ == "__main__":
+    pass
+
