a88bff94fc6255d7097883fb238687171abeed94
Extrogenesis <extrogenesis@gmail.com>
2020-02-21 13:22:50 -0500

    init: save html file to samples folder

--- capture_reddit.py
+++ capture_reddit.py
@@ -1,5 +1,22 @@
+import requests
+
 """
 1. Curl Reddit home page
 2. Save to samples folder
+TODO - check status code for http and https
 """
 
+URL = 'https://www.reddit.com'
+URL_HTTP = URL.replace('https', 'http')
+
+
+def is_status_200():  # returns first link with status code 200; prefer https
+    pass
+
+
+def get_home_page():  # returns the content from link passed form def is_status_200
+    pass
+
+
+def save_content():  # saves content into samples dir
+    pass
